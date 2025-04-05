67a50d96026822cf0c33c15dd0c8dd390495e2a5
DongHyun Kim <bainstud@naver.com>
2025-04-05 23:54:14 +0900

    Time: 6 ms (93.05%), Space: 65.2 MB (38.22%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    if (intervals.length === 0) return [];
+
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    let result = [];
+    let current = intervals[0];
+
+    for (let i = 1; i < intervals.length; i++) {
+        let next = intervals[i];
+        if (current[1] >= next[0]) {
+            current[1] = Math.max(current[1], next[1]);
+        } else {
+            result.push(current);
+            current = next;
+        }
+    }
+
+    result.push(current);
+    return result;
+};
